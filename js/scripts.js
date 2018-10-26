77f2f2c4bc9e15d340528431495c295a87498f1c
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-26 13:16:01 -0700

    add countToFuntionNumber funtion

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,42 @@ function divisibleByThree(number){
   return false;
 }
 
-//findZeroInInput
-var re = /0\d/
-function findZeroInInput(number){
-  var numberToString = number.toString()
-  var placeZero = numberToString.match(re);
-  return placeZero;
+// function countToNumber (number) {
+//   var numArray = [];
+//   for (var i = 0; i <= number; i++) {
+//     numArray.push(i);
+//   }
+//   return numArray.join();
+//   console.log(numbArray);
+// }
+
+function countToNumber (number) {
+  var numArray = [];
+  for (var i = 0; i <= number; i++) {
+  } if (numArray === 0) {
+    numArray.push("Beep")
+  } else if (numArray === 1) {
+    numArray.push("Boop")
+  } else {
+    numArray.push(i)
+  }  // numArray.push(i);
+  return numArray.join();
+  console.log(numbArray);
 }
-//
+
+function joinNumber(number){
+  var placeholder = number.join('');
+}
+
+// run the number through count to number loop and search for index positions for 0 and 1
+// or
+//return the input which converts specs and join together
+
+// //findZeroInInput
+// var re = /0\d/
+// function findZeroInInput(number){
+//   var numberToString = number.toString()
+//   var placeZero = numberToString.match(re);
+//   return placeZero;
+// }
+// //
