2f1886fd99a64fa9d5a8219d50f4c633eee9f19e
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-26 16:13:52 -0700

    clean scripts.js and remove unnescessary codes.

--- js/scripts.js
+++ js/scripts.js
@@ -37,33 +37,3 @@ function changeNumber (number) {
   });
   return replaceNumber;
 }
-// function countToNumber (number) {
-//   var numArray = [];
-//   for (var i = 0; i <= number; i++) {
-//   } if (numArray === 0) {
-//     numArray.push("Beep")
-//   } else if (numArray === 1) {
-//     numArray.push("Boop")
-//   } else {
-//     numArray.push(i)
-//   }  // numArray.push(i);
-//   return numArray.join();
-//   console.log(numbArray);
-// }
-
-function joinNumber(number){
-  var placeholder = number.join('');
-}
-
-// run the number through count to number loop and search for index positions for 0 and 1
-// or
-//return the input which converts specs and join together
-
-// //findZeroInInput
-// var re = /0\d/
-// function findZeroInInput(number){
-//   var numberToString = number.toString()
-//   var placeZero = numberToString.match(re);
-//   return placeZero;
-// }
-// //
