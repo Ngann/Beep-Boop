74183f66e2c24ef7e46c011260044d503d6f0655
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-26 19:03:24 -0700

    still working on code

--- js/scripts.js
+++ js/scripts.js
@@ -14,46 +14,19 @@ function replaceOne(number) {
 
 function divisibleByThree(number){
   var remainder = number % 3
-  if (remainder === 0) {
-    return "this is divisible by 3!"
+  if (remainder === 0 && number === !0 ) {
+    return true;
   }
   return false;
 }
 
-// function countToNumber (number) {
-//   var numArray = [];
-//   for (var i = 0; i <= number; i++) {
-//     numArray.push(i);
-//   }
-//   return numArray;
-//   // console.log(numbArray);
-// }
-//
-// function countToNumber (number) {
-//   var numArray = [];
-//   for (var i = 0; i <= number; i++) {
-//    if (number[i] === 0) {
-//      i = "Beep";
-//    } else if (number[i] === 1) {
-//      i = "Boop";
-//    } else {
-//      i ;
-//      debugger;
-//    }
-//   }
-//    numArray.push(i);
-// }
-//
-function test(abcd) {
-   for (var i = 0; i < abcd.length; i++) {
-    if (abcd[i] === "A")
-    {
-      return i+1;
-    }
+function countToNumber (number) {
+  var numArray = [];
+  for (var i = 0; i <= number; i++) {
+    numArray.push(i);
   }
+  return numArray;
 }
-
-
 
 function changeNumber (number) {
   var replaceNumber = [];
@@ -62,4 +35,50 @@ function changeNumber (number) {
     replaceNumber.push("test");
   });
   return replaceNumber;
+}
+
+//do we know our methods?
+function testThis (number) {
+  var numberInput = number;
+  var numberSearchOne = numberInput.replace('1', 'beep');
+  var numberSearchZero = numberSearchOne.replace('0', 'boop');
+  var numberArray = numberSearchZero.split();
+  var numberJoin = numberArray.join('+');
+  return numberJoin;
+}
+
+//find all number in an array divisible by 3
+// all number in an array divisible by three will say Hey!
+function allNumberDivisibleByThree (number){
+  var allArray = [];
+  for (var i = 0; i <= number; i++) {
+    if (divisibleByThree(number)) {
+      // allArray.push("sayHey");
+      return "sayHey" ;
+    } else if (number === 0) {
+      return "beep" ;
+    } else if (number === 1) {
+      return "boop"
+    } else {
+      return i;
+    }
+    // allArray.push(i);
+  }
+  // return allArray;
+  debugger;
+  console.log(divisibleByThree);
+  // console.log(allArray);
+}
+
+function testOne(number){
+  if (divisibleByThree(number)) {
+    // allArray.push("sayHey");
+    return "sayHey" ;
+  } else if ( number === 1 ) {
+    return "beep" ;
+  } else if (number === 0) {
+    return "boop"
+  } else {
+    return number;
+  }
 }
