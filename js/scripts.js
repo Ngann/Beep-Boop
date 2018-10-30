280f170b16f95bf034f29cc92ab96e9ac6f6f503
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-29 17:38:56 -0700

    refractor code

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,12 @@ function countToNumberInput (number) {
   return numArray;
 }
 
- function checkForOne(number) {
-   var string = number.toString();
-   var one = string.includes("1");
-   return one;
-
- }
-
- function checkForZero(number) {
-   var string = number.toString();
-   var one = string.includes("0");
-   return one;
-
- }
-
  function numberToOutput(number){
    if ( number > 0 && (number%3) === 0) {
      return "I'm sorry, Dave. I'm afraid I can't do that";
-   } else if (checkForOne(number) === true) {
+   } else if (number.toString().includes("1")) {
      return "Boop";
-   } else if (checkForZero(number) === true) {
+   } else if (number.toString().includes("0")) {
      return "Beep";
    } else  {
       return number;
